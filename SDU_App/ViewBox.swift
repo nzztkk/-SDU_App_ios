7afdcaa5a2073d3a34977c77a56ca9f7bcd28f8f
zn <nzakimm@gmail.com>
2024-09-17 02:59:36 +0500

    add lazycolumn

--- SDU_App/ViewBox.swift
+++ SDU_App/ViewBox.swift
@@ -9,18 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentWeek: Int = 1 // Текущая неделя
+    @State private var isWeekSelectorVisible: Bool = true // Контроль видимости панели с неделями
 
     var body: some View {
         TabView {
             // Экран с расписанием
             NavigationView {
-                VStack {
-                    // Добавляем отдельный компонент для выбора недель
-                    WeekSelectorView(currentWeek: $currentWeek)
+                VStack(spacing: 0) {
+                    // Панель с неделями и датой
+                    if isWeekSelectorVisible {
+                        WeekSelectorView(currentWeek: $currentWeek)
+                            .zIndex(1) // Поднимаем над остальными элементами
+                            .transition(.move(edge: .top))
+                            .animation(.easeInOut(duration: 0.3), value: isWeekSelectorVisible) // Анимация скрытия
+                    }
                     
                     // Основной экран с расписанием
-                    SchedulePage()
-                        .padding(.top, 10)
+                    ScrollView {
+                        SchedulePage()
+                            .padding(.top, 10)
+                            .background(GeometryReader { proxy in
+                                Color.clear
+                                    .onChange(of: proxy.frame(in: .global).minY) { newValue in
+                                        withAnimation {
+                                            // Если пользователь прокручивает вверх - показываем панель, вниз - скрываем
+                                            isWeekSelectorVisible = newValue > 0
+                                        }
+                                    }
+                            })
+                    }
                 }
                 .navigationBarTitle("schedule_p".syswords, displayMode: .inline) // Надпись Расписание сверху
                 .toolbar {
@@ -55,10 +72,7 @@ struct ContentView: View {
                 Label("deadlines_p".syswords, systemImage: "flame.fill")
             }
         }
-        
     }
-
-    
 }
 
 #Preview {
